5e0679f8b07b831c8c4d58dad71d2eeab326c06c
Abdul Kareem <abdulkareemzahir9599@gmail.com>
2026-04-13 17:00:53 +0530

    refactor: use asyncHandler for sign-up route and add authHandler middleware

---
+++ src/middlewares/authHandler.js
@@ -0,0 +1,21 @@
+const authHandler = (req, res, next) => {
+  const { token } = req.cookies;
+
+  if (!token) {
+    const error = new Error("Unauthorized");
+    error.statusCode = 401;
+    return next(error);
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.id;
+    next();
+  } catch (err) {
+    const error = new Error("Invalid token");
+    error.statusCode = 401;
+    return next(error);
+  }
+};
+
+export default authHandler;
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { signUp } from "../controllers/auth.controller.js";
 import { signUpValidator } from "../validators/auth.validator.js"
+import asyncHandler from "../middlewares/asyncHandler.js";
 
 const router = Router();
 
-router.post('/sign-up', signUpValidator, signUp);
+router.post('/sign-up', signUpValidator, asyncHandler(signUp));
 
 export default router;
