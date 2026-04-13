41704cd5b9ef3049f2881ffdfe0ac3d2dc3c98c9
Abdul Kareem <abdulkareemzahir9599@gmail.com>
2026-04-13 15:14:56 +0530

    add morgan middleware for logging requests

--- src/app.js
+++ src/app.js
@@ -3,10 +3,12 @@ import express from "express";
 import errorHandler from "./middlewares/errorHandler.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import morgan from "morgan";
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to the Bank Transaction System API" });
